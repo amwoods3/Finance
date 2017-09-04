6806e8933b6105d2c6ae092b06b4dacda96f33ae
amwoods3 <amwoods3@cougars.ccis.edu>
2017-09-03 17:57:22 -0700

    Use quotations to identify descriptions, allowing for transaction dates to come after description

--- AccountManager.cpp
+++ AccountManager.cpp
@@ -17,6 +17,52 @@ namespace AccountManager {
         }
         return splits;
     }
+
+    std::vector<std::string> split_grouping(const std::string & s,
+                                            char splitter) {
+        std::vector<std::string> splits;
+        std::string sub;
+        bool in_grouping = false;
+        char grouping_char = ' ';
+        for (int i = 0; i < s.size(); ++i) {
+            if (in_grouping) {
+                // If we are grouping, we want to group everything
+                // verbatim. We do not want to ingore spaces, etc.
+                // This allows for long description and things to
+                // follow descriptions, such as transaction dates
+                if (s[i] == grouping_char) {
+                    grouping_char = ' ';
+                    splits.push_back(sub);
+                    sub = "";
+                } else {
+                    // copy verbatim
+                    sub += s[i];
+                }
+            } else {
+                if (s[i] == splitter) {
+                    splits.push_back(sub);
+                    sub = "";
+                } else if (s[i] == '\'' || s[i] == '"') {
+                    // We may be looking at a description or such
+                    // we have to start grouping
+                    in_grouping = true;
+                    grouping_char = s[i];
+                } else {
+                sub += s[i];
+                }
+            }
+        }
+        if (in_grouping) {
+            // we have a problem,
+            // TODO: Handle this error
+        }
+        if (sub.size() > 0) {
+            splits.push_back(sub);
+        }
+        return splits;
+    }
+
+    
     Account create(const std::string & account) {
         std::vector<std::string> account_components = split(account);
         for (int i = 0; i < account_components.size(); ++i) {
@@ -38,11 +84,8 @@ namespace AccountManager {
     }
 
     std::string get_comment(std::vector<std::string> command, int start) {
-        std::string comment = "";
-        for (int i = start; i < command.size(); ++i) {
-            comment += command[i] + ' ';
-        }
-        return comment;
+        // this should be now only one part
+        return command[start];
     }
     void add(Account & ac, std::vector<std::string> add_command) {
         // assume the command starts from 2 (after the add command)
@@ -58,9 +101,10 @@ namespace AccountManager {
         std::string comment = get_comment(take_command, 3);
         ac.take_amount(take_command[2], comment);
     }
+    
     void perform_command(const std::string & command,
                          AccountMap &accounts) {
-        std::vector<std::string> command_parts = split(command);
+        std::vector<std::string> command_parts = split_grouping(command);
         if (command_parts[0] == "create") {
             Account t = create(command_parts);
             accounts.insert(AccountPair(t.name(), t));
--- AccountManager.h
+++ AccountManager.h
@@ -15,6 +15,11 @@ typedef std::map<std::string, Account>::iterator AccountIterator;
 
 namespace AccountManager {
     std::vector<std::string> split(const std::string & to_split, char splitter=' ');
+
+    // split grouping allows for splitting, and grouping based on
+    // either single or double quotes
+    std::vector<std::string> split_grouping(const std::string & to_split,
+                                   char splitter=' ');
     Account create(const std::string & account_information);
     Account create(const std::vector<std::string> & account_components);
     void perform_command(const std::string & command, AccountMap &accounts);
